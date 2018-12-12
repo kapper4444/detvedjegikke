672f7730bf1d0f8760ae932efe220d257dc0ea8d
Kasper Tandrup <kaspertandrup5@gmail.com>
2018-12-12 13:44:44 +0100

    New version
    Fixed bug and added features

---
+++ project1/Assets/enemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyController : MonoBehaviour {
+
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agentE;
+
+	// Use this for initialization
+	void Start () {
+
+        agentE = GetComponent<NavMeshAgent>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmosSelected () {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
